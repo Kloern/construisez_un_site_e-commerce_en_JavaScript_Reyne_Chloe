2b3f8062de9eae317c60834c56892518757e48db
Chloé <chloe.reyne@hotmail.fr>
2022-12-23 18:59:03 +0100

    page produits

--- front/js/product.js
+++ front/js/product.js
@@ -1,19 +1,100 @@
-// déclaration des variables du fichier JSON à insérer dans ma fonction fetch
-fetch('http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926')
-    .then(function (result) {
-        if (result.ok) {
-            return result.json()
+//récupération de l'id du produit
+const url = new URL(window.location.href);
+const idProduct = url.searchParams.get("id");
+
+//récupération des articles de l'API
+fetch("http://localhost:3000/api/products/" + idProduct)
+.then(function (res) {
+    if (res.ok) {
+        return res.json();
+    }
+})
+.then(function(value) {
+    console.log(value);
+
+    //récupération de l'image
+    const imageAlt = document.createElement("img");
+    imageAlt.setAttribute("src", value.imageUrl);
+    imageAlt.setAttribute("alt", value.altTxt);
+    const img = document.querySelector(".item__img");
+    img.appendChild(imageAlt);
+
+    //récupération du titre
+    const nameProduct = document.querySelector("#title");
+    nameProduct.innerText = value.name;
+    
+    //récupération du prix
+    const priceProduct = document.querySelector("#price");
+    priceProduct.innerText = value.price;
+
+    //récupération de la description
+    const descriptionProduct = document.querySelector("#description");
+    descriptionProduct.innerText = value.description;
+
+    //récupération des couleurs
+    const colorsProduct = document.querySelector("#colors");
+    colorsProduct.innerText = value.colors;
+    for (let i = 0; i < value.colorsProduct.length; i++) {
+        const color = document.createElement("option");
+        color.setAttribute("value", value.colorsProduct[i]);
+        color.innerText = value.colorsProduct[i];
+        colorsProduct.appendChild(colorsProduct);
+    }
+})
+.catch(function (err) {
+    console.log(err);
+});
+
+//récupération couleur et quantité
+const colorsProduct = document.querySelector("#colors");
+const colors = "";
+colorsProduct.addEventListener("input", function (e) {
+    colors = e.target.value;
+});
+
+let number = 0;
+document.querySelector("#quantity").addEventListener("input", function (e) {
+    number = e.target.value;
+});
+
+
+//stockage de la quantité, de la couleur et de l'id dans le local storage
+document.querySelector("#addToCart").addEventListener("click", function () {
+    if (check(number, color)) {
+        const item = constructItem(id, color);
+        const kanapCart = getCart ();
+
+        if (kanapCart[item] > 0) {
+            kanapCart[item] = Number(kanapCart[item]) + Number(number);
+        } else {
+            kanapCart[item] = number;
         }
-    })
-    .then(function (products) {
-        products.get(function(el, i) {
-            items.innerHTML += `<a href="./product.html?id=107fb5b75607497b96722bda5b504926">
-            <article>
-              <img src="kanap01.jpeg" alt="Lorem ipsum dolor sit amet, Kanap name1">
-              <h3 class="productName">Kanap Sinopé</h3>
-              <p class="productDescription">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-              "altTxt": "Photo d'un canapé bleu, deux places</p>
-            </article>
-            </a>`
-        })
-    });
+
+        saveCart(kanapCart);
+    }
+});
+
+//vérification si une couleur est selectionée et si quantité valide
+function check(number, color) {
+    return number > 0 && Number.isInteger(+number) && !color == "";
+}
+
+//définission du format de l'élément a enregistrer dans le local storage
+function constructItem(id, color) {
+    return [id, color];
+}
+
+//renvoi Json ou un tableau vide s'il y en a pas
+function getCart() {
+    const cart = localStorage.getItem("kanapCart");
+    if (cart != null) {
+        return JSON.parse(cart);
+    } else {
+        return {};
+    }
+}
+
+//sauvegarde de la carte dans le localstorage
+function saveCart(cart) {
+    localStorage.setItem("kanapCart", JSON.stringify(cart));
+}
--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,42 @@
-// déclaration des variables des dépendances
+//homepage
+// todo list card
+const card = document.getElementsByClassName("items");
 
+fetch("http://localhost:3000/api/products")
+  .then(function (result) {
+    if (result.ok) {
+      return result.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+    const section = document.querySelector("#items");
 
-// les imports
+    for (let i = 0; i < value.length; i++) {
+        const articleNew = document.createElement("article");
 
+        const imageAlt = document.createElement("img");
+        imageAlt.setAttribute("src", value[i].imageUrl);
+        imageAlt.setAttribute("alt", value[i].altTxt);
+        const img = document.querySelector(".item_img");
+        articleNew.appendChild(imageAlt);
 
-// déclaration des variables du fichier JSON à insérer dans ma fonction fetch
+        const title = document.createElement("h3");
+        title.setAttribute("class", "productName");
+        title.innerText = value[i].name;
+        articleNew.appendChild(title);
+
+        const description = document.createElement("p");
+        description.setAttribute("class", "productDescription");
+        description.innerText = value[i].description;
+        articleNew.appendChild(description);
+
+        const lien = document.createElement("a");
+        lien.setAttribute("href", "./product.html?id=" + value[i]._id);
+        lien.appendChild(articleNew);
+        section.appendChild(lien);
+    }
+  })
+  .catch(function (err) {
+    console.log(err);
+  })
